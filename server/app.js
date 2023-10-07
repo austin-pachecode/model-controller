b0970439cf7917fae1b1af4862dd91cb2b4394bc
Austin Yce <austin.pachecode@gmail.com>
2023-10-07 18:51:56 -0400

    API Call breathing

--- server/app.js
+++ server/app.js
@@ -1,22 +1,7 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
-var LED = new Gpio(4, 'out'); //use GPIO pin 4, and specify that it is output
 
-
-function blinkLED() { //function to start blinking
-  if (LED.readSync() === 0) { //check the pin state, if the state is 0 (or off)
-    LED.writeSync(1); //set pin state to 1 (turn LED on)
-  } else {
-    LED.writeSync(0); //set pin state to 0 (turn LED off)
-  }
-}
-
-function endBlink() { //function to stop blinking
-  clearInterval(blinkInterval); // Stop blink intervals
-  LED.writeSync(0); // Turn LED off
-  LED.unexport(); // Unexport GPIO to free resources
-}
+const GPIO_PI = require('pigpio').Gpio;
 
  //stop blinking after 5 seconds
 var app = express();
@@ -26,8 +11,23 @@ var hostname = '192.168.0.41';
 var port = 9000;
 
 app.post('/led', function(req, res) {
-    setInterval(blinkLED, 250); //run the blinkLED function every 250ms
-    setTimeout(endBlink, 5000); //stop blinking after 5 seconds
+    const ledPin = req.body.ledPin;
+    let mode = req.body.mode;
+    if(mode == 'INPUT'){
+        mode = GPIO_PI.INPUT;
+    } else {
+        mode = GPIO_PI.OUTPUT;
+    }
+    const led = new GPIO_PI(ledPin, {mode: mode});
+
+    setInterval(() => {
+      led.pwmWrite(dutyCycle);
+    
+      dutyCycle += 5;
+      if (dutyCycle > 255) {
+        dutyCycle = 0;
+      }
+    }, 20);
     console.log('receiving data ...');
     console.log('body is ',req.body);
     res.send(req.body);
