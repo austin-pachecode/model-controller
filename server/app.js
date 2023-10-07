1ae880da0f9a5f4e5dd6c87fef0e9dedf930233c
Austin Yce <austin.pachecode@gmail.com>
2023-10-07 18:16:49 -0400

    Trying LED

--- server/app.js
+++ server/app.js
@@ -1,14 +1,21 @@
-const http = require('http');
- 
-const hostname = '127.0.0.1';
-const port = 3000;
- 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+var hostname = '192.168.0.41';
+var port = 9000;
+
+app.post('/led', function(req, res) {
+    console.log('receiving data ...');
+    console.log('body is ',req.body);
+    res.send(req.body);
 });
- 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+app.get('/', (req, res) => {
+  res.send('root')
+})
+// start the server
+app.listen(port, hostname);
+console.log('Server started! At http://localhost:' + port);
