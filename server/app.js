981a2671c497f880250cd2ca93a5ca0b9d585b2a
Austin Yce <austin.pachecode@gmail.com>
2023-10-08 11:29:47 -0400

    Updating API's to more dynamic modes

--- server/app.js
+++ server/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 
 const GPIO_PI = require('pigpio').Gpio;
+const ledFunctions = require('./led.js');
+
 
  //stop blinking after 5 seconds
 var app = express();
@@ -13,16 +15,47 @@ var port = 9000;
 var ledList = [];
 
 function makeStruct(led, cycle) {
-  return {led, cycle};
+  return {"led": led, "cycle": cycle};
 }
 
 
 app.get('/ledsOFF', function(req, res) {
   console.log('ledsOFF: ', ledList, ledList[0]);
   ledList.forEach(led => {
+    console.log('led: ', led);
     led.cycle = clearInterval(led.cycle);
     led.led.pwmWrite(0);
   });
+  res.send('ledsOFF');
+});
+
+app.get('/ledsOFF/v2', function(req, res) {
+  console.log('ledsOFF: ', ledList, ledList[0]);
+  ledList.forEach(led => {
+    console.log('led: ', led);
+
+    if(led.mode == LED_ACTION.CYCLE){
+      led.action = clearInterval(led.action);
+    }
+    led.led.pwmWrite(0);
+  });
+  res.send('ledsOFF');
+});
+
+app.post('/led/v2', function(req, res) {
+  
+  let mode = req.body.mode;
+  if(mode == 'INPUT'){
+      mode = GPIO_PI.INPUT;
+  } else {
+      mode = GPIO_PI.OUTPUT;
+  }
+
+  const led = ledFunctions.createLed(req.body.ledPin, mode, req.body.action);
+  ledList.push(led);
+  console.log('receiving data ...', ledList);
+  console.log('body is ',req.body);
+  res.send(req.body);
 });
 
 
